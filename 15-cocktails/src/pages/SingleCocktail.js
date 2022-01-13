7b8edddd89bf265313359d6fdb8ed4f8b54acc61
FuChih <t19887348@hotmail.com>
2022-01-13 10:46:29 +0800

    15-cocktails

--- 15-cocktails/src/pages/SingleCocktail.js
+++ 15-cocktails/src/pages/SingleCocktail.js
@@ -27,14 +27,34 @@ function SingleCocktail() {
             strIngredient2,
             strIngredient3,
             strIngredient4,
-            strIngredient5
+            strIngredient5,
+            strIngredient6,
+            strIngredient7,
+            strIngredient8,
+            strIngredient9,
+            strIngredient10,
+            strIngredient11,
+            strIngredient12,
+            strIngredient13,
+            strIngredient14,
+            strIngredient15,
           } = drinks[0]
           const ingredients = [
             strIngredient1,
             strIngredient2,
             strIngredient3,
             strIngredient4,
-            strIngredient5
+            strIngredient5,
+            strIngredient6,
+            strIngredient7,
+            strIngredient8,
+            strIngredient9,
+            strIngredient10,
+            strIngredient11,
+            strIngredient12,
+            strIngredient13,
+            strIngredient14,
+            strIngredient15,
           ]
           const newCocktail = {
             name,
